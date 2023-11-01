4ce55c8b64d04373c853a594c520c79583efc4b9
Dhanaji Yadav <dhanaji.yadav@mmltd.co.za>
2023-11-01 20:34:59 +0530

    firebase hosting setup complete

--- lib/presentation/widgets/player_row_tile.dart
+++ lib/presentation/widgets/player_row_tile.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:team_builder/constants/paddings.dart';
 import 'package:team_builder/core/dbutil.dart';
 import 'package:team_builder/models/entity/player.dart';
 import 'package:team_builder/models/type/player_type.dart';
@@ -17,31 +16,54 @@ class PlayerRowTile extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ListTile(
-      leading: Icon(player.playerType.icon),
-      title: Row(
-        children: [
-          SizedBox(width: 150, child: Text(player.name)),
-          SizedBox(width: 100, child: Text('PR : ${player.playerRating}%')),
-          SizedBox(width: 100, child: Text('CR : ${player.captaincyRating}%')),
-          paddingLarge,
-          IconButton(
-            onPressed: () => navigateToAddPlayerScreen(context: context),
-            icon: const Padding(
-              padding: EdgeInsets.all(8.0),
-              child: Icon(Icons.edit),
+    const textStyle = TextStyle(
+      fontSize: 13,
+      color: Colors.black87,
+    );
+
+    return Row(
+      mainAxisSize: MainAxisSize.max,
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        Icon(
+          player.playerType.icon,
+          color: Colors.black54,
+        ),
+        Text(
+          player.name,
+          style: textStyle.copyWith(
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        Text(
+          'PR : ${player.playerRating}%',
+          style: textStyle,
+        ),
+        Text(
+          'CR : ${player.captaincyRating}%',
+          style: textStyle,
+        ),
+        IconButton(
+          onPressed: () => navigateToAddPlayerScreen(context: context),
+          icon: const Padding(
+            padding: EdgeInsets.all(8.0),
+            child: Icon(
+              Icons.edit,
+              color: Colors.black54,
             ),
           ),
-          paddingLarge,
-          IconButton(
-            onPressed: deletePlayer,
-            icon: const Padding(
-              padding: EdgeInsets.all(8.0),
-              child: Icon(Icons.delete),
+        ),
+        IconButton(
+          onPressed: deletePlayer,
+          icon: const Padding(
+            padding: EdgeInsets.all(8.0),
+            child: Icon(
+              Icons.delete,
+              color: Colors.black54,
             ),
           ),
-        ],
-      ),
+        ),
+      ],
     );
   }
 
